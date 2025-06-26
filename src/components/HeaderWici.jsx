b0b0329427aca62a42a5ed231d113f853d8bc039
Eltunas16 <reynadoa1601@gmail.com>
2025-06-26 12:50:35 -0500

    Avances - Integracion de patrocinadores - COICIT

--- src/components/HeaderWici.jsx
+++ src/components/HeaderWici.jsx
@@ -80,7 +80,7 @@ const Header = () => {
                     <ul className="absolute top-full left-0 mt-2 min-w-[160px] rounded-lg bg-gradient-to-br from-white to-gray-100 shadow-xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-50 border border-gray-200">
                       <li>
                         <a
-                          href="/wici/Call-For-Paper-WICI-UTPVE2025-Español.pdf"
+                          href="/Call-For-Paper-WICI-UTPVE2025-Español.pdf"
                           target="_blank"
                           rel="noopener noreferrer"
                           className="block px-5 py-3 text-sm text-[#022843] hover:bg-[#e0b02e]/90 hover:text-white transition-all rounded-t-lg"
@@ -90,7 +90,7 @@ const Header = () => {
                       </li>
                       <li>
                         <a
-                          href="/wici/Call-For-Paper-WICI-UTPVE2025-English.pdf"
+                          href="/Call-For-Paper-WICI-UTPVE2025-English.pdf"
                           target="_blank"
                           rel="noopener noreferrer"
                           className="block px-5 py-3 text-sm text-[#022843] hover:bg-[#e0b02e]/90 hover:text-white transition-all rounded-b-lg"
@@ -212,7 +212,7 @@ const Header = () => {
                     <a
                       onClick={handleLinkClick}
                       className="block text-sm hover:text-[#e0b02e]"
-                      href="/wici/Call-For-Paper-WICI-UTPVE2025-Español.pdf"
+                      href="/Call-For-Paper-WICI-UTPVE2025-Español.pdf"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
@@ -223,7 +223,7 @@ const Header = () => {
                     <a
                       onClick={handleLinkClick}
                       className="block text-sm hover:text-[#e0b02e]"
-                      href="/wici/Call-For-Paper-WICI-UTPVE2025-English.pdf"
+                      href="/Call-For-Paper-WICI-UTPVE2025-English.pdf"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
